a901b4061405c832e0fb454312f7d3642a2de347
Sergio Rodríguez <sergio.andre2709@gmail.com>
2024-01-24 11:53:02 -0500

    Se ha instalado Axios y ademas hemos aprendido a usar con react las API's y Se hara un archivo CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL 
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    fetch(`${API_URL}assets`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCriptos(data.data)
+      })
+      .catch(() => {
+        console.error("La peticion fallo")
+      })
+  })
+
+  if (!criptos) return <span>Cargando...</span>
+
   return (
-    <h1>Hola Mundo</h1>
+    <>
+      <h1>Listado de criptomonedas</h1>
+      <ol>
+        { 
+          criptos.map(({id, name, priceUsd}) => (
+            <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+          )) 
+        }
+      </ol>
+    </>
   )
 }
 
